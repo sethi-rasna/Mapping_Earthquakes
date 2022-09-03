80025673806fe796e7c149405efec90a2ff3ee93
Rasna <sethi.rasna@gmail.com>
2022-09-03 15:55:04 -0400

    adding files to repo

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 //We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -32,12 +32,19 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/sethi-rasna/Mapping_Earthquakes/main/Simple_Map/static/js/majorAirports.json"
+let torontoData = "https://raw.githubusercontent.com/sethi-rasna/Mapping_Earthquakes/main/Simple_Map/static/js/torontoRoutes.json"
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+   color: "#ffffa1",
+   weight: 2,
+   onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline " + feature.properties.airline + "</h3><hr><h3> Destination: " + feature.properties.dst + "</h3>"); 
+   }
+})
+    .addTo(map);
 });
 
